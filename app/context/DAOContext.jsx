71a4e98a8002924bbbbc538656d2e97d3ca2f04b
TheWalker <adriahernandez.2001@gmail.com>
2022-12-21 13:37:26 +0100

    Moved wallet func + gestion de errors + toast + status conver + store

--- app/context/DAOContext.jsx
+++ app/context/DAOContext.jsx
@@ -1,18 +1,30 @@
-import { useState, useEffect, createContext } from "react"
-import { ethers } from "ethers"
-import { toast } from "react-toastify"
-import fs from "fs"
 // TODO: Cambiar el voting period a 300 o algo asi para que sea una horita 12 sec = 1 block
+import { useState, useEffect, createContext, useContext } from "react"
+import { ethers } from "ethers"
+import { db } from "../backend/firebase"
+import { doc, setDoc, getDocs, collection } from "firebase/firestore"
 
+import { Web3Context } from "./Web3Context"
+
+import { toast } from "react-toastify"
 import toastConfig from "../constants/toastConfig.json"
 import "react-toastify/dist/ReactToastify.css"
 
 import governorContractABI from "../constants/GovernorContract.json"
 import boxContractABI from "../constants/Box.json"
 import contractAddressJSON from "../constants/networkMapping.json"
-import proposalsFile from "../constants/proposals.json"
 
 const chainId = process.env.NEXT_PUBLIC_CHAIN_ID
+const proposalState = [
+    "Pending",
+    "Active",
+    "Canceled",
+    "Defeated",
+    "Succeeded",
+    "Queued",
+    "Expired",
+    "Executed",
+]
 
 const governorContractAddress = contractAddressJSON[chainId].GovernorContract[0]
 const boxContractAddress = contractAddressJSON[chainId].Box[0]
@@ -20,111 +32,95 @@ const boxContractAddress = contractAddressJSON[chainId].Box[0]
 export const DAOContext = createContext()
 
 export const DAOProvider = ({ children }) => {
-    const [currentAccount, setCurrentAccount] = useState("")
-
-    // WALLET --------------------------------------------------------------------------------------
-
-    const checkIfWalletIsConnected = async () => {
-        try {
-            if (!ethereum) {
-                toast.error(`Install Metamask`, toastConfig)
-            }
-
-            const accounts = await ethereum.request({ method: "eth_accounts" })
-
-            if (accounts.length) {
-                setCurrentAccount(accounts[0])
-                console.log(`Connecting to ${accounts[0]}`)
-            } else {
-                console.log("No accounts found")
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    const connectWallet = async () => {
-        try {
-            if (!ethereum) return alert("Please install MetaMask.")
-
-            const accounts = await ethereum.request({ method: "eth_requestAccounts" })
-
-            setCurrentAccount(accounts[0])
-        } catch (error) {
-            console.log(error)
-
-            throw new Error("No ethereum object")
-        }
-    }
+    const { currentAccount } = useContext(Web3Context)
 
     // PROPOSE --------------------------------------------------------------------------------------
 
     const createPropose = async (args, functionToCall, proposalDescription) => {
-        const governor = createEthereumContract(
-            governorContractAddress,
-            governorContractABI,
-            currentAccount
-        )
-        const box = createEthereumContract(boxContractAddress, boxContractABI, currentAccount)
+        try {
+            const governor = createEthereumContract(governorContractAddress, governorContractABI)
+            const box = createEthereumContract(boxContractAddress, boxContractABI)
 
-        const encodedFunctionCall = box.interface.encodeFunctionData(functionToCall, args)
+            const encodedFunctionCall = box.interface.encodeFunctionData(functionToCall, args)
 
-        console.log(`Proposing ${functionToCall} on ${box.address} with ${args}`)
-        console.log(`Proposal Description:\n  ${proposalDescription}`)
-        toast.info(`Proposing with proposal description:\n  ${proposalDescription}`, toastConfig)
+            console.log(`Proposing ${functionToCall} on ${box.address} with ${args}`)
+            console.log(`Proposal Description:\n  ${proposalDescription}`)
+            toast.info(
+                `Proposing with proposal description:\n  ${proposalDescription}`,
+                toastConfig
+            )
 
-        const proposeTx = await governor.propose(
-            [box.address], // Contracts that we are using
-            [0], // ETH that we pass
-            [encodedFunctionCall], // Encoded parameters and function
-            proposalDescription // Text that we use ase description
-        )
+            const proposeTx = await governor.propose(
+                [box.address], // Contracts that we are using
+                [0], // ETH that we pass
+                [encodedFunctionCall], // Encoded parameters and function
+                proposalDescription // Text that we use ase description
+            )
 
-        const proposeReceipt = await proposeTx.wait(1)
-        const proposalId = proposeReceipt.events[0].args.proposalId
-        console.log(`Proposed with proposal ID:\n  ${proposalId}`)
-        toast.info(`Proposed with proposal ID:\n  ${proposalId}`, toastConfig)
+            const proposeReceipt = await proposeTx.wait(1)
+            const proposalId = proposeReceipt.events[0].args.proposalId
 
-        const proposalState = await governor.state(proposalId)
-        const proposalSnapShot = await governor.proposalSnapshot(proposalId)
-        const proposalDeadline = await governor.proposalDeadline(proposalId)
+            console.log(`Proposed with proposal ID:\n  ${proposalId}`)
+            toast.success(`Proposed with proposal ID:\n  ${proposalId}`, toastConfig)
 
-        // Save the proposalId
-        storeProposalId(proposalId)
+            const proposalStateId = await governor.state(proposalId)
+            const proposalSnapShot = await governor.proposalSnapshot(proposalId)
+            const proposalDeadline = await governor.proposalDeadline(proposalId)
 
-        // The state of the proposal. 1 is not passed. 0 is passed.
-        console.log(`Current Proposal State: ${proposalState}`)
-        // What block # the proposal was snapshot
-        console.log(`Current Proposal Snapshot: ${proposalSnapShot}`)
-        // The block number the proposal voting expires
-        console.log(`Current Proposal Deadline: ${proposalDeadline}`)
+            // Save the proposal
+            await storeProposal(
+                proposalId,
+                proposalStateId,
+                proposalSnapShot,
+                proposalDeadline,
+                currentAccount,
+                proposalDescription,
+                functionToCall,
+                args
+            )
 
-        console.log("--------------------------------------")
+            // The state of the proposal. 1 is not passed. 0 is passed.
+            console.log(`Current Proposal State: ${proposalState[proposalStateId]}`)
+            // What block # the proposal was snapshot
+            console.log(`Current Proposal Snapshot: ${proposalSnapShot}`)
+            // The block number the proposal voting expires
+            console.log(`Current Proposal Deadline: ${proposalDeadline}`)
+
+            console.log("--------------------------------------")
+        } catch (error) {
+            console.log(error.code)
+            console.log(error.message)
+
+            toast.error("Algo ha ido mal en la realización de la propuesta", toastConfig)
+        }
     }
+
     // STATUS --------------------------------------------------------------------------------------
 
     const checkStatus = async (proposalId) => {
-        const governor = createEthereumContract(
-            governorContractAddress,
-            governorContractABI,
-            currentAccount
-        )
+        try {
+            const governor = createEthereumContract(governorContractAddress, governorContractABI)
 
-        const stateTx = await governor.state(proposalId)
+            const stateTx = await governor.state(proposalId)
 
-        console.log(stateTx)
+            console.log(`Current state is ${proposalState[stateTx]}`)
+            toast.info(`Current state is ${proposalState[stateTx]}`, toastConfig)
 
-        console.log("--------------------------------------")
+            console.log("--------------------------------------")
+        } catch (error) {
+            console.log(error.code)
+            console.log(error.message)
+
+            toast.error("Algo ha ido mal en la comprovación del status", toastConfig)
+        }
     }
 
+    // RETURN --------------------------------------------------------------------------------------
     return (
         <DAOContext.Provider
             value={{
-                checkIfWalletIsConnected,
-                connectWallet,
                 createPropose,
                 checkStatus,
-                currentAccount,
             }}
         >
             {children}
@@ -132,7 +128,7 @@ export const DAOProvider = ({ children }) => {
     )
 }
 
-const createEthereumContract = (contractAddress, contractABI, currentAccount) => {
+const createEthereumContract = (contractAddress, contractABI) => {
     const provider = new ethers.providers.Web3Provider(ethereum)
     const signer = provider.getSigner()
     const contract = new ethers.Contract(contractAddress, contractABI, signer)
@@ -140,17 +136,32 @@ const createEthereumContract = (contractAddress, contractABI, currentAccount) =>
     return contract
 }
 
-const storeProposalId = (proposalId, chainId) => {
-    let proposals
+const storeProposal = async (
+    proposalId,
+    state,
+    snapshot,
+    deadline,
+    proposer,
+    description,
+    functionToCall,
+    args
+) => {
+    try {
+        await setDoc(doc(db, "proposals", proposalId), {
+            proposalId,
+            state,
+            snapshot,
+            deadline,
+            proposer,
+            description,
+            functionToCall,
+            args,
+        })
+        toast.success(`Your proposal has been registered`, toastConfig)
+    } catch (error) {
+        console.log(error.code)
+        console.log(error.message)
 
-    if (fs.existsSync(proposalsFile)) {
-        proposals = JSON.parse(fs.readFileSync(proposalsFile, "utf8"))
-    } else {
-        proposals = {}
-        proposals[chainId] = []
+        toast.error(`There's been an error storing your proposal, contact support`, toastConfig)
     }
-
-    proposals[chainId].push(proposalId.toString())
-
-    fs.writeFileSync(proposalsFile, JSON.stringify(proposals), "utf8")
 }
