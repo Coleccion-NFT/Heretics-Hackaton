0b5d87e386acd743cba6667cd29045342b3b8040
TheWalker <adriahernandez.2001@gmail.com>
2022-12-22 23:44:09 +0100

    Loader changes need to do the todo

--- app/components/Loader.jsx
+++ app/components/Loader.jsx
@@ -1,7 +1,8 @@
 const Loader = ({ h, w }) => {
     return (
-        <div className="flex justify-center items-center h-full w-full">
-            <div className={`animate-spin rounded-full h-${h} w-${w} border-b-2 border-red-700`} />
+        <div className="flex justify-center items-center">
+            <div className={`animate-spin rounded-full h-24 w-24 border-b-2 border-red-700`} />
+            {/* TODO: Poner ciertos tamaños i ya */}
         </div>
     )
 }
