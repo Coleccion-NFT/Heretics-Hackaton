2e3c95dea879a65eda3596bde254bb6f683ce524
Miquel romeu goñi <68238351+MiquelRomeu@users.noreply.github.com>
2022-12-23 21:58:02 +0100

    comentarios fuera

--- app/components/ChatMessage.jsx
+++ app/components/ChatMessage.jsx
@@ -34,8 +34,3 @@ export const ChatMessage = ({ message, authorUid, userUid, photoURL }) => {
 }
 
 export default ChatMessage
-
-// <div className="w-fit h-fit bg-white flex pl-3 pr-4 py-1 items-center rounded-2xl mt-1">
-//     <img src={logo} className="w-7 h-auto mr-5" />
-//     {message}
-// </div>
