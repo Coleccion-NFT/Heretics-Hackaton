17b1cec186ef43f4f90485c42f0210bdd0082cd0
TheWalker <adriahernandez.2001@gmail.com>
2022-12-23 05:12:13 +0100

    Fix on mynfts

--- app/pages/mynfts.js
+++ app/pages/mynfts.js
@@ -1,93 +1,83 @@
-import { useState } from "react"
+import { useState, useEffect, useContext } from "react"
 import { NFTCard } from "../components"
 
+import { Web3Context } from "../context/Web3Context"
+
+import contractAddressJSON from "../constants/networkMapping.json"
+
 const MyNfts = () => {
-    const [wallet, setWalletAddress] = useState("")
+    const { checkIfWalletIsConnected, connectWallet, currentAccount } = useContext(Web3Context)
+
+    const creatorNftContractAddress =
+        contractAddressJSON[process.env.NEXT_PUBLIC_CHAIN_ID].CreatorNft[0]
+    const apiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY
+    const apiKeyMoralis = process.env.NEXT_PUBLIC_MORALIS_API_KEY
+
     const [NFTs, setNFTs] = useState([])
-    const [chain, setChain] = useState("eth")
 
     const fetchNFTs = async () => {
-        console.log("Fetching nfts...")
-        var requestOptions = {
-            method: "GET",
-            redirect: "follow",
-        }
+        try {
+            if (currentAccount) {
+                console.log("Fetching nfts...")
 
-        const apiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY
+                var requestOptions = {
+                    method: "GET",
+                    headers: { accept: "application/json", "X-API-Key": `${apiKeyMoralis}` },
+                }
 
-        let baseURL
-        switch (chain) {
-            case "eth":
-                baseURL = `https://eth-mainnet.alchemyapi.io/nft/v2/${apiKey}/getNFTs`
-                break
-            case "polygon":
-                baseURL = `https://polygon-mainnet.g.alchemy.com/nft/v2/${apiKey}/getNFTs`
-                break
-            case "goerli":
-                baseURL = `https://eth-goerli.g.alchemy.com/nft/v2/${apiKey}/getNFTs`
-                break
-            default:
-                baseURL = `https://eth-mainnet.alchemyapi.io/nft/v2/${apiKey}/getNFTs`
-                break
-        }
-        console.log("BaseURL: " + baseURL)
+                let baseURL = `https://deep-index.moralis.io/api/v2`
 
-        let nfts
-        console.log("Fetching nfts owned by address...")
+                let nfts
+                let fetchURL = `${baseURL}/${currentAccount}/nft?chain=bsc%20testnet&format=decimal&token_addresses=0x171b14a6505923e29ba195d7bc1a31a546fecd0b&normalizeMetadata=true`
 
-        const fetchURL = `${baseURL}?owner=${wallet}&pageSize=100&withMetadata=true`
-        console.log("Fetching URL: " + fetchURL)
+                nfts = await fetch(fetchURL, requestOptions)
+                    .then((data) => data.json())
+                    .catch((error) => console.error({ error }))
 
-        nfts = await fetch(fetchURL, requestOptions)
-            .then((data) => data.json())
-            .catch((error) => console.log("error", error))
-
-        if (nfts) {
-            console.log("NFTs:", nfts)
-            setNFTs(nfts.ownedNfts)
+                if (nfts) {
+                    setNFTs(nfts.result)
+                }
+            }
+        } catch (error) {
+            console.log(error)
         }
     }
 
-    return (
-        <div className="w-full h-screen flex flex-col items-center justify-start pl-8 pr-4 py-14">
-            <div className="flex flex-col w-full justify-center items-center gap-y-2">
-                <input
-                    type="text"
-                    placeholder="Add your wallet address"
-                    value={wallet}
-                    onChange={(e) => {
-                        setWalletAddress(e.target.value)
-                    }}
-                ></input>{" "}
-                <label>
-                    Choose a chain:
-                    <select value={chain} onChange={(e) => setChain(e.target.value)}>
-                        <option value="eth">Ethereum</option>
-                        <option value="polygon">Polygon</option>
-                        <option value="goerli">Goerli</option>
-                    </select>
-                </label>
-                <button
-                    className={
-                        "disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"
-                    }
-                    onClick={async (e) => {
-                        e.preventDefault()
-                        console.log("Clicked!")
-                        await fetchNFTs()
-                    }}
-                >
-                    Let's go!{" "}
-                </button>
-            </div>
+    useEffect(() => {
+        // Anonymus function
+        ;(async () => {
+            await checkIfWalletIsConnected()
+            await fetchNFTs()
+        })()
+    }, [currentAccount])
 
-            <div className="flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center">
-                {NFTs.length &&
-                    NFTs.map((nft, index) => {
-                        return <NFTCard key={index} nft={nft} />
-                    })}
-            </div>
-        </div>
+    return (
+        <>
+            {!currentAccount ? (
+                <div className="h-screen flex flex-col items-center justify-center">
+                    <button
+                        className="bg-black text-white px-9 py-1.5 my-5 w-80 text-center"
+                        type="button"
+                        onClick={async (e) => {
+                            await connectWallet()
+                        }}
+                    >
+                        Conéctate a Metamask
+                    </button>
+                </div>
+            ) : (
+                <>
+                    <div className="w-full h-screen flex flex-col items-center justify-start pl-8 pr-4 py-14">
+                        <div className="flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-5 justify-center">
+                            {NFTs?.length &&
+                                NFTs.map((nft, index) => {
+                                    return <NFTCard key={index} nft={nft} />
+                                })}
+                        </div>
+                    </div>
+                </>
+            )}
+        </>
     )
 }
 
